65005c6df942dc43bd16ce6dcbfd328d8a6f9300
Lawrence Walters <lwalters@lyonscg.com>
2020-03-22 21:25:53 +0100

    berlin germany italy plot

--- histogram-new-cases-all.R
+++ histogram-new-cases-all.R
@@ -22,19 +22,24 @@ variable_names <- list(
   "italy_increase" = "Italy"
 )
 
-
 variable_labeller <- function(variable,value){
   return(variable_names[value])
 }
 
 # Berlin
-ggplot(subset(dflong,date > Sys.Date() - 20), aes(x=date,y=value)) +
+ggplot(subset(dflong,date > Sys.Date() - 20), 
+       aes(x=date,
+           y=value,
+           fill=variable)) +
   scale_x_date(date_labels = "%Y-%m-%d",
-               date_breaks = "3 day")+
-  theme(axis.text.x = element_text(angle = -90, hjust = 1))+
-  geom_col()+
+               date_breaks = "7 day",
+               name = "Date")+
+  theme(axis.text.x = element_text(angle = -90, hjust = 1),
+        legend.position="none")+
+  geom_col(colour="black")+
   scale_y_continuous("New Cases per day")+
   facet_wrap(~variable, 
              scales="free_y", 
-             labeller= variable_labeller)
+             labeller= variable_labeller)+
+  scale_fill_brewer(palette="Dark2")
 
