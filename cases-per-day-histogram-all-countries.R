0845f0602a1da7cbd23e05d775e90ec071e5fe81
Lawrence Walters <lwalters@lyonscg.com>
2020-03-24 23:50:11 +0100

    dynamic title, date range

--- cases-per-day-histogram-all-countries.R
+++ cases-per-day-histogram-all-countries.R
@@ -1,4 +1,5 @@
 # case increase per day graphs
+number_of_days <- 19
 
 # Loading
 library(reshape2)
@@ -26,20 +27,35 @@ variable_labeller <- function(variable,value){
   return(variable_names[value])
 }
 
-# Berlin
-ggplot(subset(dflong,date > Sys.Date() - 20), 
-       aes(x=date,
-           y=value,
-           fill=variable)) +
-  scale_x_date(date_labels = "%Y-%m-%d",
-               date_breaks = "7 day",
-               name = "Date")+
-  theme(axis.text.x = element_text(angle = -90, hjust = 1),
-        legend.position="none")+
-  geom_col(colour="black")+
-  scale_y_continuous("New Cases per day")+
-  facet_wrap(~variable, 
-             scales="free_y", 
-             labeller= variable_labeller)+
-  scale_fill_brewer(palette="Dark2")
+weekend_labeller <- function(variable, value) {
+  return("hello")
+}
 
+x_end <- dflong %>% 
+  group_by(date) %>% 
+  top_n(1, date) %>%
+  pull(date)
+
+
+ggplot(subset(dflong, date > Sys.Date() - number_of_days),
+       aes(x = date,
+           y = value,
+           fill = variable)) +
+  scale_x_date(date_labels = "%m.%d",
+               breaks = "3 days",
+               minor_breaks = "1 day",
+               name = "Date",
+               #labels = weekend_labeller
+               #limits = c(Sys.Date() - number_of_days,Sys.Date()),
+               #expand=c(0,0)
+               ) +
+  theme(axis.text.x = element_text(angle = -90, vjust = 0.3),
+        legend.position = "none") +
+  geom_col(colour = "black") +
+  scale_y_continuous("New Cases per day") +
+  facet_wrap(~ variable,
+             scales = "free_y",
+             labeller = variable_labeller) +
+  scale_fill_brewer(palette = "Dark2") +
+  ggtitle(paste("New Cases per Day",Sys.Date()),
+          subtitle = paste("Note different scales! In the last", number_of_days, "days"))
--- cases-per-day-histogram-berlin.R
+++ cases-per-day-histogram-berlin.R
@@ -1,4 +1,5 @@
 # case increase per day graphs
+number_of_days <- 21
 
 # Loading
 library(reshape2)
@@ -10,10 +11,13 @@ df <- mutate(df, berlin_increase = berlin_cases - lag(berlin_cases))
 df <- mutate(df, date = as.Date(date, '%Y-%m-%d'))
 
 # Berlin
-ggplot(subset(df,date > Sys.Date() - 20), aes(x=date)) +
+ggplot(subset(df,date > Sys.Date() - number_of_days), aes(x=date)) +
   scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "3 day")+
   theme(axis.text.x = element_text(angle = -90, hjust = 1))+
   geom_col(aes(y=berlin_increase, group=2),
            position = "dodge", fill = "grey50", colour = "black")+
-  scale_y_continuous("New Cases per day")
+  scale_y_continuous("New Cases per day") +
+  ggtitle("Berlin New Cases per Day",
+          subtitle = paste("in the last",number_of_days, "days"))
+  
 
