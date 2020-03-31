6cdf6f8142b724592527e7f0aa8aa04fb28fb571
Lawrence Walters <lwalters@lyonscg.com>
2020-03-31 19:00:55 +0200

    age distro berlin

---
+++ cases-age-distribution-Berlin.R
@@ -0,0 +1,40 @@
+# Age distribution of cases over time in Berlin
+
+# sorting tool
+age_group_sort <- c(c("0-4","5-9","10-14","15-19","20-24","25-29","30-39","40-49","50-59","60-69","70-79","80+","unk"))
+dates <- c("2020-03-11","2020-03-18","2020-03-30")
+
+# Loading
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+library(broom)
+
+df<-read.csv("data\\age-distribution.csv", header = TRUE)
+df <- filter(df,date %in% dates)
+# get date as actual date object, and weekend calcs
+df <- mutate(df, 
+             date = as.Date(date, '%Y-%m-%d'), 
+             wday = as.POSIXlt(date)$wday, 
+             wkday = ifelse(wday == 0 | wday == 6,0.9,1))
+latest_data_date = max(df$date)
+
+# get percentage distribution
+df <- df %>%
+  group_by(date,place) %>%
+  mutate(valuePct = value / sum(value) * 100)
+
+
+df[order(df$date,factor(df$ageGroup,levels=age_group_sort)),]
+
+ggplot(df[order(df$date),],
+       aes(x = ageGroup,
+           y = valuePct,
+           fill = date)) +
+  geom_bar(stat = 'identity', colour = "black", 
+           position="dodge2") + #position_dodge(width = NULL, preserve = "total")) +
+  scale_y_continuous("Percent of Total Caess") +
+  scale_x_discrete(limits=age_group_sort) +
+  labs(title = "Berlin age distribution over time",
+       subtitle = paste("Data from",min(df$date),"to",max(df$date)),
+       caption = "Sources: berlin.de (Berlin)")
